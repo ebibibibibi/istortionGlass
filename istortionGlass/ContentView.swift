9dd368c6d20c98071c9e39a9030e6beace7f0b65
えび🦐 <91860481+ebibibibibi@users.noreply.github.com>
2025-08-08 14:08:14 +0900

    Initial Commit

---
+++ istortionGlass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  istortionGlass
+//
+//  Created by KotomiTakahashi on 2025/08/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ istortionGlass/istortionGlassApp.swift
@@ -0,0 +1,17 @@
+//
+//  istortionGlassApp.swift
+//  istortionGlass
+//
+//  Created by KotomiTakahashi on 2025/08/08.
+//
+
+import SwiftUI
+
+@main
+struct istortionGlassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
